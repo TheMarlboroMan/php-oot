0081c5292ad829414b45d266e3143b65d5855ce0
Dani <marlborometal@gmail.com>
2018-02-17 20:30:01 +0100

    More stuff

--- api.php
+++ api.php
@@ -1,20 +1,14 @@
 <?php
 ini_set('display_errors', 1);
 
-//Load renoir engine...
 require("../renoir_engine/autoload.php");
-
-//Load REST API.
 require_once("../rest_api/autoload.php");
 
-//Load application fundamentals...
-//TODO: When done: https://help.github.com/articles/removing-sensitive-data-from-a-repository/
-\Rest_api\Api_bootstrap::set_api_config('config/oot.ini');
-
-require("src/api.class.php");
-
-//Logic.
-$instance=new \OOT\Api;
-\Rest_api\Api_bootstrap::run($instance);
-
-//curl -X POST|GET|PUT|DELETE "http://localhost/oot/api/shit?hola=1&cosa=2" -H "Content-type: text/plain" -d "Hello"
+try {
+	require("src/api.class.php");
+	\Rest_api\Api_bootstrap::set_api_config('config/oot.ini');
+	\Rest_api\Api_bootstrap::run(new \OOT\Api);
+}
+catch(\Exception $e) {
+	die('Error: '.$e->getMessage());
+}
